cccd4544a8cccdc93030fcd82c347e96cdc0f2f5
Mahmoud Sayed <ma7moudsayed111@gmail.com>
2025-03-17 15:43:47 +0200

    created user_app

--- watchmate/settings.py
+++ watchmate/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'watchlist_app',
     'rest_framework',
+    'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
@@ -131,7 +132,8 @@ REST_FRAMEWORK = {
     # ],
     
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
         # 'rest_framework.authentication.SessionAuthentication',
+        
     ]
 }
